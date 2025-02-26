7ff355f342cce1aafc22be3b5d1864b6fd9e5001
bardikeduard <51379469+bardikeduard@users.noreply.github.com>
2025-02-26 13:56:23 +0300

    2: rewrite pipe creation

--- 2/bg_array.h
+++ 2/bg_array.h
@@ -8,49 +8,56 @@
 #define BG_PROC_ARR_INIT_SIZE 10
 #define BG_PROC_ARR_GROW_COEFF 2
 
-struct bg_array {
-	size_t bg_size;
-	size_t bg_capacity;
-	pid_t *children;
+struct pid_array {
+	size_t pa_size;
+	size_t pa_capacity;
+	pid_t *pa_children;
 };
 
 static inline int
-bg_array_init(struct bg_array *arr)
+pid_array_init(struct pid_array *arr)
 {
 	assert(arr != NULL);
-	arr->bg_size = 0;
-	arr->bg_capacity = BG_PROC_ARR_INIT_SIZE;
-	arr->children = calloc(arr->bg_capacity, sizeof(pid_t));
-	if (arr->children == NULL) {
+	arr->pa_size = 0;
+	arr->pa_capacity = BG_PROC_ARR_INIT_SIZE;
+	arr->pa_children = calloc(arr->pa_capacity, sizeof(pid_t));
+	if (arr->pa_children == NULL) {
 		return 1;
 	}
 
 	return 0;
 }
 
+static inline void
+pid_array_free(struct pid_array *arr)
+{
+	assert(arr != NULL);
+	free(arr->pa_children);
+}
+
 static inline int
-bg_array_realloc(struct bg_array *arr)
+pid_array_realloc(struct pid_array *arr)
 {
 	assert(arr != NULL);
 
 	size_t new_capacity = 0;
 
-	if (arr->bg_size * BG_PROC_ARR_GROW_COEFF < arr->bg_capacity && arr->bg_size > BG_PROC_ARR_INIT_SIZE) {
-		new_capacity = arr->bg_capacity / BG_PROC_ARR_GROW_COEFF;
+	if (arr->pa_size * BG_PROC_ARR_GROW_COEFF < arr->pa_capacity && arr->pa_size > BG_PROC_ARR_INIT_SIZE) {
+		new_capacity = arr->pa_capacity / BG_PROC_ARR_GROW_COEFF;
 	}
 
-	if (arr->bg_size == arr->bg_capacity) {
-		new_capacity = arr->bg_capacity * BG_PROC_ARR_GROW_COEFF;
+	if (arr->pa_size == arr->pa_capacity) {
+		new_capacity = arr->pa_capacity * BG_PROC_ARR_GROW_COEFF;
 	}
 
 	if (new_capacity != 0) {
-		pid_t *new_children = realloc(arr->children, sizeof(pid_t) * new_capacity);
+		pid_t *new_children = realloc(arr->pa_children, sizeof(pid_t) * new_capacity);
 		if (new_children == NULL) {
 			return 1;
 		}
 
-		arr->children = new_children;
-		arr->bg_capacity = new_capacity;
+		arr->pa_children = new_children;
+		arr->pa_capacity = new_capacity;
 		return 0;
 	}
 
@@ -58,16 +65,16 @@ bg_array_realloc(struct bg_array *arr)
 }
 
 static inline int
-bg_array_wait_nonblock(struct bg_array *arr)
+pid_array_wait_nonblock(struct pid_array *arr)
 {
 	assert(arr != NULL);
 
-	for (size_t i = 0; i < arr->bg_size;) {
-		if (waitpid(arr->children[i], NULL, WNOHANG) > 0) {
-			--arr->bg_size;
+	for (size_t i = 0; i < arr->pa_size;) {
+		if (waitpid(arr->pa_children[i], NULL, WNOHANG) > 0) {
+			--arr->pa_size;
 
-			if (i < arr->bg_size) {
-				memmove(arr->children + i, arr->children + i + 1, sizeof(pid_t) * (arr->bg_size - i));
+			if (i < arr->pa_size) {
+				memmove(arr->pa_children + i, arr->pa_children + i + 1, sizeof(pid_t) * (arr->pa_size - i));
 			}
 		}
 		else {
@@ -75,20 +82,34 @@ bg_array_wait_nonblock(struct bg_array *arr)
 		}
 	}
 
-	return bg_array_realloc(arr);
+	return pid_array_realloc(arr);
 }
 
 static inline int
-bg_array_push(struct bg_array *arr, pid_t child)
+pid_array_wait_and_free(struct pid_array *arr)
 {
 	assert(arr != NULL);
-	arr->children[arr->bg_size++] = child;
-	return bg_array_realloc(arr);
+
+	int last_exitcode = 0;
+
+	for (size_t child_idx = 0; child_idx < arr->pa_size; ++child_idx) {
+		int status;
+		waitpid(arr->pa_children[child_idx], &status, 0);
+
+		if (WIFEXITED(status)) {
+			last_exitcode = WEXITSTATUS(status);
+		}
+	}
+
+	pid_array_free(arr);
+
+	return last_exitcode;
 }
 
-static inline void
-bg_array_free(struct bg_array *arr)
+static inline int
+pid_array_push(struct pid_array *arr, pid_t child)
 {
 	assert(arr != NULL);
-	free(arr->children);
+	arr->pa_children[arr->pa_size++] = child;
+	return pid_array_realloc(arr);
 }
--- 2/solution.c
+++ 2/solution.c
@@ -6,10 +6,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
-#include <sys/wait.h>
 #include <fcntl.h>
-
-#define container_of(ptr, type, member) (type*)((char*)(ptr) - offsetof(type, member))
 
 struct exec_result {
 	int need_exit;
@@ -59,239 +56,173 @@ execute_cmd(const struct expr *expression)
 }
 
 static int
-wait_children_and_free(pid_t *children, size_t children_count)
+is_expr_logical(const struct expr *e)
 {
-	assert(children != NULL);
-
-	int last_exitcode = 0;
-
-	for (size_t child_idx = 0; child_idx < children_count; ++child_idx) {
-		int status;
-		waitpid(children[child_idx], &status, 0);
-
-		if (WIFEXITED(status)) {
-			last_exitcode = WEXITSTATUS(status);
-		}
-	}
-	free(children);
-
-	return last_exitcode;
+	assert(e != NULL);
+	return e->type == EXPR_TYPE_AND || e->type == EXPR_TYPE_OR;
 }
 
-static void
-close_pipes(int *pipes_fds, size_t size)
+static int is_expr_logical_last(const struct expr *e)
 {
-	assert(pipes_fds != NULL);
-
-	for (size_t fd_idx = 0; fd_idx < size; ++fd_idx) {
-		close(pipes_fds[fd_idx]);
-	}
-}
-
-static int
-create_pipes(int **pipes_fds, size_t cmd_count)
-{
-	assert(pipes_fds != NULL);
-
-	int *new_pipes_fds = calloc(2 * cmd_count, sizeof(int));
-	if (new_pipes_fds == NULL) {
-		return 1;
-	}
-
-	for (size_t i = 0; i < cmd_count - 1; ++i) {
-		if (pipe(new_pipes_fds + 2 * i + 1) != 0) {
-			close_pipes(new_pipes_fds + 1, i);
-			free(new_pipes_fds);
-			return 1;
-		}
-
-		int tmp = new_pipes_fds[2 * i + 1];
-		new_pipes_fds[2 * i + 1] = new_pipes_fds[2 * (i + 1)];
-		new_pipes_fds[2 * (i + 1)] = tmp;
-	}
-
-	*pipes_fds = new_pipes_fds;
-	return 0;
+	assert(e != NULL);
+	return e->next == NULL || is_expr_logical(e->next);
 }
 
 static struct exec_result
-execute_pipeline(struct expr *pipeline_start, size_t cmd_count,
+execute_pipeline(struct expr *pipeline_start,
 	const char *out_file, enum output_type out_type, int need_wait)
 {
 	assert(pipeline_start != NULL);
 
-	size_t child_size = 0;
-	pid_t *children_pids = calloc(cmd_count, sizeof(pid_t));
-	if (children_pids == NULL) {
-		puts("calloc error\n");
+	struct pid_array children_pids;
+	if (pid_array_init(&children_pids) != 0) {
+		dprintf(STDERR_FILENO, "calloc error\n");
 		return make_result(0, 1, NULL, 0);
 	}
 
-	int *pipes_fds;
-	if (create_pipes(&pipes_fds, cmd_count) != 0) {
-		puts("pipe creation error\n");
-		free(children_pids);
-		return make_result(0, 1, NULL, 0);
-	}
-
-	pipes_fds[0] = STDIN_FILENO;
-	pipes_fds[2 * cmd_count - 1] = STDOUT_FILENO;
-
-	struct expr *expression = pipeline_start;
 	size_t i = 0;
+	int pipe_fds[3] = {STDIN_FILENO, STDOUT_FILENO, -1};
+	struct expr *expression = pipeline_start;
 
-	while (i < cmd_count) {
+	while (expression != NULL && !is_expr_logical(expression)) {
 		if (expression->type != EXPR_TYPE_COMMAND) {
 			expression = expression->next;
 			continue;
 		}
 
-		if (strcmp("cd", expression->cmd.exe) == 0) {
-			if (cmd_count == 1 && handle_cd_command(expression) != 0) {
-				puts("cd error\n");
+		if (!is_expr_logical_last(expression)) {
+			if (pipe(pipe_fds + 1) != 0) {
+				dprintf(STDERR_FILENO, "pipe creation error %zu\n", i);
+				return make_result(0, 0, NULL, 0);
+			}
 
-				close_pipes(pipes_fds, 2 * (cmd_count - 1));
-				free(pipes_fds);
-				free(children_pids);
+			int tmp = pipe_fds[1];
+			pipe_fds[1] = pipe_fds[2];
+			pipe_fds[2] = tmp;
+		}
+
+		if (strcmp("cd", expression->cmd.exe) == 0 && children_pids.pa_size == 0 && is_expr_logical_last(expression)) {
+			if (handle_cd_command(expression) != 0) {
+				dprintf(STDERR_FILENO, "cd error\n");
+				pid_array_free(&children_pids);
+
+				if (pipe_fds[0] != STDIN_FILENO) {
+					close(pipe_fds[0]);
+				}
+
+				if (pipe_fds[1] != STDOUT_FILENO) {
+					close(pipe_fds[1]);
+				}
 
 				return make_result(0, -1, NULL, 0);
 			}
 		}
 		else if (strcmp("exit", expression->cmd.exe) == 0) {
-			if (i == cmd_count - 1) {
-				close_pipes(pipes_fds + 1, 2 * (cmd_count - 1));
-				free(pipes_fds);
-				wait_children_and_free(children_pids, child_size);
+			if (expression->next == NULL || is_expr_logical(expression->next)) {
+				int is_one_cmd = (children_pids.pa_size == 0);
+				pid_array_wait_and_free(&children_pids);
+
+				if (pipe_fds[0] != STDIN_FILENO) {
+					close(pipe_fds[0]);
+				}
+
+				if (pipe_fds[1] != STDOUT_FILENO) {
+					close(pipe_fds[1]);
+				}
 
 				if (expression->cmd.arg_count != 0) {
 					char *end;
 					int return_code = (int) strtol(expression->cmd.args[0], &end, 10);
-					return make_result(cmd_count == 1, return_code, NULL, 0);
+					return make_result(is_one_cmd, return_code, NULL, 0);
 				}
 
-				return make_result(cmd_count == 1, 0, NULL, 0);
+				return make_result(is_one_cmd, 0, NULL, 0);
 			}
 		}
 		else {
 			pid_t child_pid = fork();
 
 			if (child_pid == -1) {
-				puts("fork error\n");
+				dprintf(STDERR_FILENO, "fork error\n");
+				pid_array_wait_and_free(&children_pids);
 
-				close_pipes(pipes_fds + 1, 2 * (cmd_count - 1));
-				free(pipes_fds);
-				wait_children_and_free(children_pids, child_size);
-
-				return make_result(1, 1, children_pids, child_size);
+				return make_result(1, 1, NULL, 0);
 			}
 
 			if (child_pid == 0) {
-				free(children_pids);
+				pid_array_free(&children_pids);
 
-				if (need_wait != 0 || i != 0) {
-					if (dup2(pipes_fds[2 * i], STDIN_FILENO) != STDIN_FILENO) {
-						puts("dup2 error\n");
-
-						close_pipes(pipes_fds, cmd_count);
-						free(pipes_fds);
-
+				if (need_wait != 0 || children_pids.pa_size > 0) {
+					if (dup2(pipe_fds[0], STDIN_FILENO) != STDIN_FILENO) {
+						dprintf(STDERR_FILENO, "dup2 error\n");
 						return make_result(1, 0, NULL, 0);
 					}
 				}
 				else {
-					close(STDIN_FILENO);
+					close(pipe_fds[0]);
 				}
 
-				int out_fd = pipes_fds[2 * i + 1];
-				if (i == cmd_count - 1 && out_type != OUTPUT_TYPE_STDOUT && out_file != NULL) {
-					out_fd = open(out_file,
-						O_CREAT | O_WRONLY | (out_type == OUTPUT_TYPE_FILE_NEW ? O_TRUNC : O_APPEND),
-						S_IRWXU | S_IRWXG | S_IRWXO
-					);
-					if (out_fd == -1) {
-						puts("out file open error\n");
+				int out_fd = pipe_fds[1];
+				if (is_expr_logical_last(expression)) {
+					if (out_fd != STDOUT_FILENO) {
+						close(out_fd);
+					}
 
-						close_pipes(pipes_fds, cmd_count);
-						free(pipes_fds);
-
-						return make_result(1, 0, NULL, 0);
+					if (out_type != OUTPUT_TYPE_STDOUT) {
+						out_fd = open(out_file,
+							O_CREAT | O_WRONLY | (out_type == OUTPUT_TYPE_FILE_NEW ? O_TRUNC : O_APPEND),
+							S_IRWXU | S_IRWXG | S_IRWXO
+						);
+						if (out_fd == -1) {
+							dprintf(STDERR_FILENO, "out file open error\n");
+							return make_result(1, 0, NULL, 0);
+						}
+					}
+					else {
+						out_fd = STDOUT_FILENO;
 					}
 				}
 
 				if (dup2(out_fd, STDOUT_FILENO) != STDOUT_FILENO) {
-					puts("dup2 error\n");
-
-					close_pipes(pipes_fds, cmd_count);
-					free(pipes_fds);
-
+					dprintf(STDERR_FILENO, "dup2 error\n");
 					return make_result(1, 0, NULL, 0);
 				}
 
-				for (size_t fd_idx = 1; fd_idx < 2 * cmd_count - 1; ++fd_idx) {
-					if (fd_idx != 2 * i + 1 && fd_idx != 2 * i) {
-						close(pipes_fds[fd_idx]);
-					}
+				if (pipe_fds[2] != -1) {
+					close(pipe_fds[2]);
 				}
-				free(pipes_fds);
 
 				execute_cmd(expression);
 				return make_result(1, 0, NULL, 0);
 			}
 
-			children_pids[child_size++] = child_pid;
+			if (pid_array_push(&children_pids, child_pid) != 0) {
+				dprintf(STDERR_FILENO, "pid_array_push error\n");
+				break;
+			}
 		}
 
+		if (pipe_fds[0] != STDIN_FILENO) {
+			close(pipe_fds[0]);
+		}
+
+		if (pipe_fds[1] != STDOUT_FILENO) {
+			close(pipe_fds[1]);
+		}
+
+		pipe_fds[0] = pipe_fds[2];
 		expression = expression->next;
-		++i;
 	}
 
-	close_pipes(pipes_fds + 1, 2 * (cmd_count - 1));
-	free(pipes_fds);
+	if (pipe_fds[0] != STDIN_FILENO) {
+		close(pipe_fds[0]);
+	}
 
 	if (need_wait) {
-		return make_result(0, wait_children_and_free(children_pids, child_size), NULL, 0);
+		return make_result(0, pid_array_wait_and_free(&children_pids), NULL, 0);
 	}
 
-	return make_result(0, 0, children_pids, child_size);
-}
-
-static struct exec_result
-execute_logical_operand(struct expr *start, const char *out_file,
-	enum output_type out_type, int need_wait)
-{
-	assert(start != NULL);
-
-	size_t cmd_count = 0;
-	struct expr *iter = start;
-
-	while (iter != NULL) {
-		switch (iter->type) {
-		case EXPR_TYPE_AND:
-		case EXPR_TYPE_OR: {
-			return execute_pipeline(start, cmd_count, out_file, out_type, need_wait);
-		}
-
-		case EXPR_TYPE_COMMAND: {
-			++cmd_count;
-			break;
-		}
-
-		default: {
-			break;
-		}
-		}
-
-		iter = iter->next;
-	}
-
-	return execute_pipeline(start, cmd_count, out_file, out_type, need_wait);
-}
-
-static int
-is_expr_logical(const struct expr *e)
-{
-	assert(e != NULL);
-	return e->type == EXPR_TYPE_AND || e->type == EXPR_TYPE_OR;
+	return make_result(0, 0, children_pids.pa_children, children_pids.pa_size);
 }
 
 static struct exec_result
@@ -306,7 +237,7 @@ execute_command_line(const struct command_line *line)
 	}
 
 	int is_last = (iter == NULL);
-	struct exec_result prev_result = execute_logical_operand(operand_start,
+	struct exec_result prev_result = execute_pipeline(operand_start,
 		is_last ? line->out_file : NULL,
 		is_last ? line->out_type : OUTPUT_TYPE_STDOUT,
 		is_last ? (line->is_background == 0) : 1
@@ -328,7 +259,7 @@ execute_command_line(const struct command_line *line)
 			}
 
 			is_last = (iter == NULL);
-			prev_result = execute_logical_operand(operand_start,
+			prev_result = execute_pipeline(operand_start,
 				is_last ? line->out_file : NULL,
 				is_last ? line->out_type : OUTPUT_TYPE_STDOUT,
 				is_last ? (line->is_background == 0) : 1
@@ -351,9 +282,9 @@ main(void)
 	struct parser *p = parser_new();
 	int last_retcode = 0;
 
-	struct bg_array bg_proc;
-	if (bg_array_init(&bg_proc) != 0) {
-		puts("bg_array_init error\n");
+	struct pid_array bg_proc;
+	if (pid_array_init(&bg_proc) != 0) {
+		dprintf(STDERR_FILENO, "init error\n");
 		parser_delete(p);
 		return 1;
 	}
@@ -376,8 +307,8 @@ main(void)
 
 			if (result.bg_pids != NULL) {
 				for (size_t i = 0; i < result.bg_count; ++i) {
-					if (bg_array_push(&bg_proc, result.bg_pids[i]) != 0) {
-						puts("bg_array_push error\n");
+					if (pid_array_push(&bg_proc, result.bg_pids[i]) != 0) {
+						dprintf(STDERR_FILENO, "bg_array_push error\n");
 						break;
 					}
 				}
@@ -385,19 +316,19 @@ main(void)
 				free(result.bg_pids);
 			}
 
-			if (bg_array_wait_nonblock(&bg_proc) != 0) {
-				puts("bg_array_wait_nonblock error\n");
+			if (pid_array_wait_nonblock(&bg_proc) != 0) {
+				dprintf(STDERR_FILENO, "bg_array_wait_nonblock error\n");
 			}
 
 			if (result.need_exit != 0) {
-				bg_array_free(&bg_proc);
+				pid_array_free(&bg_proc);
 				parser_delete(p);
 				return result.return_code;
 			}
 		}
 	}
 
-	bg_array_free(&bg_proc);
+	pid_array_free(&bg_proc);
 	parser_delete(p);
 	return last_retcode;
 }
